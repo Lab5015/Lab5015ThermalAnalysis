23a9674954bb7582b2cdd44248ae6cdfd933893b
abenagli <andrea.benaglia@cern.ch>
2021-05-26 15:04:44 +0200

    first import

---
+++ scanTemperature.py
@@ -0,0 +1,14 @@
+import sys
+import time
+
+sys.path.append("/home/cmsdaq/Lab5015Utils/")
+from Lab5015_utils import SMChiller
+
+SMC = SMChiller()
+tempList = [35,30,25,20,15,10]
+
+for temp in tempList:
+    print("set temp to: ",temp)
+    SMC.write_set_temp(temp)
+    time.sleep(900)
+SMC.set_state(0)
